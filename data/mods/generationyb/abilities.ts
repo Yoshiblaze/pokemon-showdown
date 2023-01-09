603b301992e7d430fcbaa6e567625b6686a7fb7f
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-01-09 17:14:43 -0500

    Update abilities.ts

--- data/mods/generationyb/abilities.ts
+++ data/mods/generationyb/abilities.ts
@@ -97,4 +97,205 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		rating: 1.5,
 		num: 123,
 	},
+	glider: {
+		onStart(pokemon) {
+			if (pokemon.side.sideConditions['tailwind']) {
+				this.boost({spe: 1}, pokemon, pokemon);
+			}
+		},
+		onDamagingHitOrder: 1,
+		onDamagingHit(damage, target, source, move) {
+			if (move.flags['wind']) {
+				this.boost({spe: 1}, target, target);
+			}
+		},
+		onAllySideConditionStart(target, source, sideCondition) {
+			const pokemon = this.effectState.target;
+			if (sideCondition.id === 'tailwind') {
+				this.boost({spe: 1}, pokemon, pokemon);
+			}
+		},
+		onAfterMoveSecondarySelf(source, target, move) {
+			if (!move || !target) return;
+			if (move.flags['wind']) {
+				this.boost({spe: 1}, source, source);
+			}
+		},
+		name: "Glider",
+		shortDesc: "After using or getting hit by a wind move, this Pokemon's Speed goes up by 1 stage.",
+		rating: 3.5,
+		num: 274,
+	},
+
+// Will do legendary abilities later
+
+	heartofcourage: {
+		onModifyAtkPriority: 5,
+		onModifyAtk(atk, attacker, defender, move) {
+			if (attacker.hasType(move.type) && attacker.hp <= attacker.maxhp / 3) {
+				this.debug('Heart of Courage boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA(atk, attacker, defender, move) {
+			if (attacker.hasType(move.type) && attacker.hp <= attacker.maxhp / 3) {
+				this.debug('Heart of Courage boost');
+				return this.chainModify(1.5);
+			}
+		},
+		name: "Heart of Courage",
+		shortDesc: "At 1/3 or less of its max HP, this Pokemon's offensive stat is 1.5x with STAB attacks.",
+		rating: 2,
+		num: 66,
+	},
+	highfashion: {
+		onBasePowerPriority: 15,
+		onBasePower(basePower, user, target, move) {
+			if (move.type === 'Normal') {
+				return this.chainModify([4915, 4096]);
+			}
+		},
+		name: "High Fashion",
+		shortDesc: "User's Normal-type attacks have 1.2x power.",
+		rating: 3.5,
+		num: 181,
+	},
+	inevitable: {
+		name: "Inevitable",
+		onDamagingHitOrder: 1,
+		onDamagingHit(damage, target, source, move) {
+			if (!target.hp && target.species.id === 'gbonawola') {
+				this.actions.useMove("Future Flames", target);
+			}
+			if (!target.hp && target.species.id === 'gbonanene') {
+				this.actions.useMove("Future Harvest", target);
+			}
+			if (!target.hp && target.species.id === 'gbonablanu') {
+				this.actions.useMove("Future Laments", target);
+			}
+			if (!target.hp && target.species.id === 'gbonazito') {
+				this.actions.useMove("Future Shock", target);
+			}
+		},
+		shortDesc: "When this Pokemon faints due to another Pokemon's move, it uses its signature future move if it's not already active.",
+		rating: 2,
+		num: 106,
+	},
+	insulttoinjury: {
+		onBasePowerPriority: 19,
+		onBasePower(basePower, attacker, defender, move) {
+			if (defender.hurtThisTurn) {
+				this.debug('BP multiplied on damaged target');
+				return this.chainModify(1.5);
+			}
+		},
+		name: "Insult to Injury",
+		shortDesc: "This Pokemon's moves deal 1.5x damage to targets that have already taken damage this turn.",
+		rating: 3.5,
+		num: 173,
+	},
+	laststand: {
+		onBasePowerPriority: 19,
+		onBasePower(basePower, attacker, defender, move) {
+			if (attacker.moveSlots.length > 1 && attacker.moveSlot.used) {
+				return this.chainModify(1.3);
+			}
+		},
+		name: "Last Stand",
+		shortDesc: "This Pokemon's moves deal 1.3x damage if it has used each of its moves at least once. Resets on switch out.",
+		rating: 3.5,
+		num: 173,
+	},
+	nevermore: {
+		onStart(pokemon) {
+			pokemon.addVolatile('nevermore');
+		},
+		condition: {
+			onStart(pokemon) {
+				this.effectState.lastMove = '';
+				this.effectState.numConsecutive = 0;
+			},
+			onTryMovePriority: -2,
+			onTryMove(pokemon, target, move) {
+				if (!move.flags['sound']) {
+					pokemon.removeVolatile('nevermore');
+					return;
+				}
+				if (move.flags['sound'] && !pokemon.volatiles['nevermore']) {
+					pokemon.addVolatile('nevermore');
+					return;
+				}
+				if (this.effectState.lastMove === move.id && pokemon.moveLastTurnResult) {
+					this.effectState.numConsecutive++;
+				} else if (pokemon.volatiles['twoturnmove']) {
+					if (this.effectState.lastMove !== move.id) {
+						this.effectState.numConsecutive = 1;
+					} else {
+						this.effectState.numConsecutive++;
+					}
+				} else {
+					this.effectState.numConsecutive = 0;
+				}
+				this.effectState.lastMove = move.id;
+			},
+			onModifyDamage(damage, source, target, move) {
+				const dmgMod = [4096, 4915, 5734, 6553, 7372, 8192];
+				const numConsecutive = this.effectState.numConsecutive > 5 ? 5 : this.effectState.numConsecutive;
+				this.debug(`Current Metronome boost: ${dmgMod[numConsecutive]}/4096`);
+				return this.chainModify([dmgMod[numConsecutive], 4096]);
+			},
+		},
+		name: "Nevermore",
+		shortDesc: "This Pokemon's sound moves gain power each time they're used, capping at 2x power.",
+		rating: 3.5,
+		num: 173,
+	},
+	powerrush: {
+		onModifyDamage(damage, source, target, move) {
+			return this.chainModify([5324, 4096]);
+		},
+		onAfterMoveSecondarySelf(source, target, move) {
+			if (source && source !== target && move && move.category !== 'Status') {
+				this.add('-ability', source, 'Power Rush');
+				this.damage(source.baseMaxhp / 10, source, source);
+			}
+		},
+		name: "Power Rush",
+		shortDesc: "This Pokemon's moves deal 1.3x damage, but this Pokemon loses 10% of its max HP every time it lands an attack.",
+		rating: 3.5,
+		num: 173,
+	},
+	reboot: {
+		onDamagingHit(damage, target, source, move) {
+			this.field.setTerrain('electricterrain');
+		},
+		name: "Reboot",
+		shortDesc: "When this Pokemon is hit by an attack, the effect of Electric Terrain begins.",
+		rating: 2.5,
+		num: 269,
+	},
+	rocketfists: {
+		onModifyMovePriority: 8,
+		onModifyMove(move, pokemon) {
+			if ((move.flags['punch'])  && move.category === 'Physical') move.category = 'Special';
+		},
+		name: "Rocket Fists",
+		shortDesc: "This Pokemon's punching moves are Special.",
+		rating: 2.5,
+		num: 269,
+	},
+	rolereversal: {
+		onModifyMovePriority: 1,
+		onModifyMove(move, attacker, defender) {
+			if (attacker.species.baseSpecies !== 'Tragichiou' || attacker.transformed) return;
+			const targetForme = (move.category === 'Special' ? 'Tragichiou-Comedy' : 'Tragichiou');
+			if (attacker.species.name !== targetForme) attacker.formeChange(targetForme);
+		},
+		isPermanent: true,
+		name: "Role Reversal",
+		shortDesc: "This Pokemon changes to its Comedy form before it uses a Special move and its Tragedy form before it uses a Physical move.",
+		rating: 4,
+		num: 176,
+	},
 };
