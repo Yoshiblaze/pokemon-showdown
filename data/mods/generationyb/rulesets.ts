1c74f71d9f76f21650649f5a03522e1ce4c1b1aa
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-01-21 15:37:12 -0500

    Update rulesets.ts

--- data/mods/generationyb/rulesets.ts
+++ data/mods/generationyb/rulesets.ts
@@ -1,4 +1,4 @@
-export const Formats: {[k: string]: FormatData} = {
+export const Rulesets: {[k: string]: ModdedFormatData} = {
 	onelinkbracelimit: {
 		effectType: 'ValidatorRule',
 		name: 'One Link Brace Limit',
