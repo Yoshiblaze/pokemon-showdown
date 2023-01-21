5669e9a3c7ee4109bd8fdf6edc6b9ed09d93228c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-01-21 15:20:35 -0500

    Update rulesets.ts

--- data/mods/generationyb/rulesets.ts
+++ data/mods/generationyb/rulesets.ts
@@ -1,39 +1,11 @@
 export const Formats: {[k: string]: FormatData} = {
-	itemclause: {
+	onelinkbracelimit: {
 		effectType: 'ValidatorRule',
-		name: 'Item Clause',
-		desc: "Prevents teams from having more than one Pok&eacute;mon with the same item",
+		name: 'One Link Brace Limit',
+		desc: "Prevents teams from having more than one Pok&eacute;mon from wearing a Link Brace.",
+		banlist: ["Link Brace > 1"],
 		onBegin() {
-			this.add('rule', 'Item Clause: Limit one of each item');
-		},
-		onValidateTeam(team) {
-			const itemTable = new Set<string>();
-			for (const set of team) {
-				const item = this.toID(set.item);
-				if (!item) continue;
-				if (itemTable.has(item)) {
-					return [
-						`You are limited to one of each item by Item Clause.`,
-						`(You have more than one ${this.dex.items.get(item).name})`,
-					];
-				}
-				itemTable.add(item);
-			}
+			this.add('rule', 'One Link Brace Limit: Limit to one Link Brace');
 		},
 	},
-	doubleitemclause: {
-		effectType: 'ValidatorRule',
-		name: 'Double Item Clause',
-		desc: "Prevents teams from having more than two Pok&eacute;mon with the same item",
-		onBegin() {
-			this.add('rule', 'Double Item Clause: Limit two of each item');
-		},
-		onValidateTeam(team) {
-			const itemTable: {[k: string]: number} = {};
-			for (const set of team) {
-				const item = this.toID(set.item);
-				if (!item) continue;
-				if (item in itemTable) {
-					if (itemTable[item] >= 2) {
-		
 };
