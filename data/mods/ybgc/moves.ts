c1697cf6ec8668c7a4c9523d913f83cf9f56ccf9
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-02-13 19:56:22 -0500

    Create moves.ts

---
+++ data/mods/ybgc/moves.ts
@@ -0,0 +1,242 @@
+export const Moves: {[k: string]: ModdedMoveData} = {
+	brine: {
+		num: 362,
+		accuracy: 100,
+		basePower: 65,
+		basePowerCallback(pokemon, target, move) {
+			if (target.hurtThisTurn) {
+				this.debug('Boosted for being damaged this turn');
+				return move.basePower * 2;
+			}
+			return move.basePower;
+		},
+		category: "Special",
+		name: "Brine",
+		shortDesc: "Power doubles if target was damaged this turn.",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		secondary: null,
+		target: "normal",
+		type: "Water",
+		contestType: "Tough",
+	},
+	assurance: {
+		num: 372,
+		accuracy: 100,
+		basePower: 65,
+		basePowerCallback(pokemon, target, move) {
+			if (target.hurtThisTurn) {
+				this.debug('Boosted for being damaged this turn');
+				return move.basePower * 2;
+			}
+			return move.basePower;
+		},
+		category: "Physical",
+		name: "Assurance",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		secondary: null,
+		target: "normal",
+		type: "Dark",
+		contestType: "Clever",
+	},
+	present: {
+		num: 217,
+		accuracy: 100,
+		basePower: 0,
+		category: "Physical",
+		name: "Present",
+		shortDesc: "40, 80, 120 power, or heals target 1/4 max HP. Always heals ally by 1/2 max HP.",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		onModifyMove(move, pokemon, target) {
+			const rand = this.random(10);
+			if (rand < 2) {
+				move.heal = [1, 4];
+				move.infiltrates = true;
+			} else if (rand < 6) {
+				move.basePower = 40;
+			} else if (rand < 9) {
+				move.basePower = 80;
+			} else {
+				move.basePower = 120;
+			}
+		},
+		onHit(target, source) {
+			if (source.side === target.side) {
+				if (!this.heal(Math.floor(target.baseMaxhp * 0.5))) {
+					this.add('-immune', target);
+				}
+			}
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+		contestType: "Cute",
+	},
+	minimize: {
+		num: 107,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		shortDesc: "Protects from damaging attacks. Contact: -1 Spe.",                      
+		name: "Minimize",
+		pp: 10,
+		priority: 4,
+		flags: {snatch: 1},
+		volatileStatus: 'minimize',
+		onPrepareHit(pokemon) {
+			return !!this.queue.willAct() && this.runEvent('StallMove', pokemon);
+		},
+		onHit(pokemon) {
+			pokemon.addVolatile('stall');
+		},
+		condition: {
+			duration: 1,
+			onStart(target) {
+				this.add('-singleturn', target, 'Protect');
+			},
+			onTryHitPriority: 3,
+			onTryHit(target, source, move) {
+				if (!move.flags['protect'] || move.category === 'Status') {
+					if (move.isZ || (move.isMax && !move.breaksProtect)) target.getMoveHitData(move).zBrokeProtect = true;
+					return;
+				}
+				if (move.smartTarget) {
+					move.smartTarget = false;
+				} else {
+					this.add('-activate', target, 'move: Protect');
+				}
+				const lockedmove = source.getVolatile('lockedmove');
+				if (lockedmove) {
+					// Outrage counter is reset
+					if (source.volatiles['lockedmove'].duration === 2) {
+						delete source.volatiles['lockedmove'];
+					}
+				}
+				if (move.flags['contact']) {
+					this.boost({spe: -1}, source, target, this.dex.getActiveMove("Minimize"));
+				}
+				return this.NOT_FAIL;
+			},
+			onHit(target, source, move) {
+				if (move.isZOrMaxPowered && move.flags['contact']) {
+					this.boost({spe: -1}, source, target, this.dex.getActiveMove("Minimize"));
+				}
+			},
+		},
+		secondary: null,
+		target: "self",
+		type: "Normal",
+		zMove: {effect: 'clearnegativeboost'},
+		contestType: "Cute",
+	},
+	doubleteam: {
+		num: 104,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Double Team",
+		shortDesc: "Boosts the user's critical hit ratio by 2 stages.",                                              
+		pp: 30,
+		priority: 0,
+		flags: {snatch: 1},
+		onHit(pokemon) {
+			pokemon.addVolatile('focusenergy');
+		},
+		secondary: null,
+		target: "self",
+		type: "Normal",
+		zMove: {effect: 'clearnegativeboost'},
+		contestType: "Cool",
+	},
+	muddywater: {
+		num: 330,
+		accuracy: 85,
+		basePower: 90,
+		category: "Special",
+		shortDesc: "30% chance to lower the foe(s) Attack by 1.",                                                                        
+		name: "Muddy Water",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, nonsky: 1},
+		secondary: {
+			chance: 30,
+			boosts: {
+				atk: -1,
+			},
+		},
+		target: "allAdjacentFoes",
+		type: "Water",
+		contestType: "Tough",
+	},                          
+	nightdaze: {
+		num: 539,
+		accuracy: 90,
+		basePower: 100,
+		category: "Special",
+		shortDesc: "20% chance to make the foe(s) flinch.",                                                                                                  
+		name: "Night Daze",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		secondary: {
+			chance: 20,
+			volatileStatus: 'flinch',
+		},
+		target: "allAdjacentFoes",
+		type: "Dark",
+		contestType: "Cool",
+	},
+	drillpeck: {
+		num: 65,
+		accuracy: 100,
+		basePower: 80,
+		category: "Physical",
+		shortDesc: "High critical hit ratio.",                                                                                                  		
+    name: "Drill Peck",
+		pp: 20,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, distance: 1},
+		critRatio: 2,                         
+		secondary: null,
+		target: "any",
+		type: "Flying",
+		contestType: "Cool",
+	},                          
+	dragonclaw: {
+		num: 337,
+		accuracy: 100,
+		basePower: 80,
+		category: "Physical",
+		shortDesc: "High critical hit ratio.",                                                                                                  		                          
+		name: "Dragon Claw",
+		pp: 15,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		critRatio: 2,                                                   
+		secondary: null,
+		target: "normal",
+		type: "Dragon",
+		contestType: "Cool",
+	},
+	xscissor: {
+		num: 404,
+		accuracy: 100,
+		basePower: 80,
+		category: "Physical",
+ 		shortDesc: "High critical hit ratio.",                                                                                                  		                                                   
+		name: "X-Scissor",
+		pp: 15,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		secondary: null,
+		critRatio: 2,                                                   
+    target: "normal",
+		type: "Bug",
+		contestType: "Cool",
+	},                          
+};
