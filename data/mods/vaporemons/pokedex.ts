30b1eb44b0bafa23bade08da47d03987c87bdfeb
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-07-14 15:38:48 -0400

    Update pokedex.ts

--- data/mods/vaporemons/pokedex.ts
+++ data/mods/vaporemons/pokedex.ts
@@ -2,7 +2,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 	screamtail: {
 		inherit: true,
 		types: ["Fairy", "Dragon"],
-		abilities: {0: "Protosynthesis", H: "Cute Charm"},
+		abilities: {0: "Protosmosis", H: "Cute Charm"},
 	},
 	crabominable: {
 		inherit: true,
@@ -109,11 +109,16 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 	},
 	vespiquen: {
 		inherit: true,
-		abilities: {0: "Pressure", 1: "Cute Charm", H: "Unnerve"},
+		types: ["Poison", "Flying"],
+		abilities: {0: "Intimidate", 1: "Cute Charm", H: "Supreme Overlord"},
+	},
+	misdreavus: {
+		inherit: true,
+		abilities: {0: "Levitate", 1: "Death Aura"},
 	},
 	mismagius: {
 		inherit: true,
-		abilities: {0: "Levitate", H: "Cute Charm"},
+		abilities: {0: "Levitate", 1: "Death Aura", H: "Cute Charm"},
 	},
 	floette: {
 		inherit: true,
@@ -125,7 +130,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 	},
 	florges: {
 		inherit: true,
-		abilities: {0: "Flower Veil", 1: "Healer", H: "Symbiosis"},
+		abilities: {0: "Grass Pelt", 1: "Healer", H: "Symbiosis"},
 	},
 	oranguru: {
 		inherit: true,
@@ -170,5 +175,324 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
 	mesprit: {
 		inherit: true,
 		abilities: {0: "Levitate", H: "Healer"},
+	},
+	bellibolt: {
+		inherit: true,
+		types: ["Electric", "Water"],
+	},
+	decidueye: {
+		inherit: true,
+		abilities: {0: "Overgrow", H: "Contrary"},
+	},
+	decidueyehisui: {
+		inherit: true,
+		types: ["Ghost", "Fighting"],
+		abilities: {0: "Overgrow", H: "Scrappy"},
+	},
+	magnemite: {
+		inherit: true,
+		abilities: {0: "Magnet Pull", 1: "Levitate", H: "Analytic"},
+	},
+	magneton: {
+		inherit: true,
+		abilities: {0: "Magnet Pull", 1: "Levitate", H: "Analytic"},
+	},
+	magnezone: {
+		inherit: true,
+		abilities: {0: "Magnet Pull", 1: "Levitate", H: "Analytic"},
+	},
+	greattusk: {
+		inherit: true,
+		abilities: {0: "Protocrysalis"},
+	},
+	sandyshocks: {
+		inherit: true,
+		abilities: {0: "Protocrysalis", H: "Sand Spit"},
+	},
+	fluttermane: {
+		inherit: true,
+		abilities: {0: "Protostasis"},
+	},
+	brutebonnet: {
+		inherit: true,
+		abilities: {0: "Protosmosis", H: "Seed Sower"},
+	},
+	irontreads: {
+		inherit: true,
+		abilities: {0: "Rune Drive"},
+	},
+	ironbundle: {
+		inherit: true,
+		abilities: {0: "Neuron Drive"},
+	},
+	ironhands: {
+		inherit: true,
+		abilities: {0: "Photon Drive"},
+	},
+	ironjugulis: {
+		inherit: true,
+		abilities: {0: "Neuron Drive"},
+	},
+	ironmoth: {
+		inherit: true,
+		abilities: {0: "Photon Drive"},
+	},
+	roaringmoon: {
+		inherit: true,
+		abilities: {0: "Protostasis"},
+	},
+	ironvaliant: {
+		inherit: true,
+		abilities: {0: "Rune Drive"},
+	},
+	arboliva: {
+		inherit: true,
+		abilities: {0: "Seed Sower", 1: "Grass Pelt", H: "Harvest"},
+	},
+	squawkabillyyellow: {
+		inherit: true,
+		abilities: {0: "Intimidate", 1: "Hustle", H: "Gale Wings"},
+	},
+	squawkabillywhite: {
+		inherit: true,
+		abilities: {0: "Intimidate", 1: "Hustle", H: "Gale Wings"},
+	},
+	calyrex: {
+		inherit: true,
+		abilities: {0: "Unnerve", H: "Grass Pelt"},
+	},
+	swablu: {
+		inherit: true,
+		abilities: {0: "Natural Cure", 1: "Gale Wings", H: "Cloud Nine"},
+	},
+	altaria: {
+		inherit: true,
+		abilities: {0: "Natural Cure", 1: "Gale Wings", H: "Cloud Nine"},
+	},
+	tropius: {
+		inherit: true,
+		abilities: {0: "Chlorophyll", 1: "Gale Wings", H: "Harvest"},
+	},
+	articuno: {
+		inherit: true,
+		abilities: {0: "Pressure", 1: "Gale Wings", H: "Snow Cloak"},
+	},
+	rotomfan: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Gale Wings"},
+	},
+	bombirdier: {
+		inherit: true,
+		abilities: {0: "Big Pecks", 1: "Gale Wings", H: "Rocky Payload"},
+	},
+	oricorio: {
+		inherit: true,
+		abilities: {0: "Dancer", 1: "Muscle Memory"},
+	},
+	oricoriopau: {
+		inherit: true,
+		abilities: {0: "Dancer", 1: "Muscle Memory"},
+	},
+	oricoriopompom: {
+		inherit: true,
+		abilities: {0: "Dancer", 1: "Muscle Memory"},
+	},
+	oricoriosensu: {
+		inherit: true,
+		abilities: {0: "Dancer", 1: "Muscle Memory", H: "Death Aura"},
+	},
+	hydreigon: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Muscle Memory"},
+	},
+	jolteon: {
+		inherit: true,
+		abilities: {0: "Volt Absorb", H: "Muscle Memory"},
+	},
+	flamigo: {
+		inherit: true,
+		abilities: {0: "Scrappy", 1: "Muscle Memory", H: "Costar"},
+	},
+	meloetta: {
+		inherit: true,
+		types: ["Psychic", "Fighting"],
+		abilities: {0: "Trace", H: "Muscle Memory"},
+	},
+	meloettapirouette: {
+		inherit: true,
+		abilities: {0: "No Guard", H: "Muscle Memory"},
+	},
+	landorus: {
+		inherit: true,
+		abilities: {0: "Sand Force", H: "Cloud Nine"},
+	},
+	hoppip: {
+		inherit: true,
+		abilities: {0: "Chlorophyll", 1: "Cloud Nine", H: "Infiltrator"},
+	},
+	skiploom: {
+		inherit: true,
+		abilities: {0: "Chlorophyll", 1: "Cloud Nine", H: "Infiltrator"},
+	},
+	jumpluff: {
+		inherit: true,
+		abilities: {0: "Chlorophyll", 1: "Cloud Nine", H: "Infiltrator"},
+	},
+	lycanroc: {
+		inherit: true,
+		abilities: {0: "Keen Eye", 1: "Sand Rush", H: "Cloud Nine"},
+	},
+	igglybuff: {
+		inherit: true,
+		abilities: {0: "Cute Charm", 1: "Competitive", H: "Cloud Nine"},
+	},
+	jigglypuff: {
+		inherit: true,
+		abilities: {0: "Cute Charm", 1: "Competitive", H: "Cloud Nine"},
+	},
+	wigglytuff: {
+		inherit: true,
+		abilities: {0: "Cute Charm", 1: "Competitive", H: "Cloud Nine"},
+	},
+	dudunsparce: {
+		inherit: true,
+		abilities: {0: "Serene Grace", 1: "Cloud Nine", H: "Rattled"},
+	},
+	dudunsparcethreesegment: {
+		inherit: true,
+		abilities: {0: "Serene Grace", 1: "Cloud Nine", H: "Rattled"},
+	},
+	cacturne: {
+		inherit: true,
+		abilities: {0: "Sand Veil", 1: "Sand Force", H: "Water Absorb"},
+	},
+	gible: {
+		inherit: true,
+		abilities: {0: "Sand Veil", 1: "Sand Force", H: "Rough Skin"},
+	},
+	gabite: {
+		inherit: true,
+		abilities: {0: "Sand Veil", 1: "Sand Force", H: "Rough Skin"},
+	},
+	garchomp: {
+		inherit: true,
+		abilities: {0: "Sand Veil", 1: "Sand Force", H: "Rough Skin"},
+	},
+	lycanrocmidnight: {
+		inherit: true,
+		abilities: {0: "Keen Eye", 1: "Sand Force", H: "No Guard"},
+	},
+	typhlosionhisui: {
+		inherit: true,
+		abilities: {0: "Blaze", H: "Death Aura"},
+	},
+	gastly: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Death Aura"},
+	},
+	haunter: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Death Aura"},
+	},
+	gengar: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Neutralizing Gas"},
+	},
+	rellor: {
+		inherit: true,
+		abilities: {0: "Compound Eyes", 1: "Sand Force", H: "Shed Skin"},
+	},
+	rabsca: {
+		inherit: true,
+		abilities: {0: "Synchronize", 1: "Sand Force", H: "Telepathy"},
+	},
+	greavard: {
+		inherit: true,
+		abilities: {0: "Pickup", 1: "Death Aura", H: "Fluffy"},
+	},
+	houndstone: {
+		inherit: true,
+		abilities: {0: "Sand Rush", 1: "Death Aura", H: "Fluffy"},
+	},
+	spiritomb: {
+		inherit: true,
+		abilities: {0: "Pressure", 1: "Death Aura", H: "Infiltrator"},
+	},
+	froslass: {
+		inherit: true,
+		abilities: {0: "Snow Cloak", 1: "Death Aura", H: "Cursed Body"},
+	},
+	houndoom: {
+		inherit: true,
+		abilities: {0: "Death Aura", 1: "Flash Fire", H: "Unnerve"},
+	},
+	voltorbhisui: {
+		inherit: true,
+		abilities: {0: "Soundproof", 1: "Seed Sower", H: "Aftermath"},
+	},
+	electrodehisui: {
+		inherit: true,
+		abilities: {0: "Soundproof", 1: "Seed Sower", H: "Aftermath"},
+	},
+	chespin: {
+		inherit: true,
+		abilities: {0: "Overgrow", H: "Seed Sower"},
+	},
+	quilladin: {
+		inherit: true,
+		abilities: {0: "Overgrow", H: "Seed Sower"},
+	},
+	chesnaught: {
+		inherit: true,
+		abilities: {0: "Overgrow", H: "Seed Sower"},
+	},
+	bounsweet: {
+		inherit: true,
+		abilities: {0: "Seed Sower", 1: "Oblivious", H: "Sweet Veil"},
+	},
+	steenee: {
+		inherit: true,
+		abilities: {0: "Seed Sower", 1: "Oblivious", H: "Sweet Veil"},
+	},
+	tsareena: {
+		inherit: true,
+		abilities: {0: "Seed Sower", 1: "Queenly Majesty", H: "Sweet Veil"},
+	},
+	leafeon: {
+		inherit: true,
+		abilities: {0: "Seed Sower", H: "Chlorophyll"},
+	},
+	diglett: {
+		inherit: true,
+		abilities: {0: "Sand Spit", 1: "Arena Trap", H: "Sand Force"},
+	},
+	dugtrio: {
+		inherit: true,
+		abilities: {0: "Sand Spit", 1: "Arena Trap", H: "Sand Force"},
+	},
+	sandile: {
+		inherit: true,
+		abilities: {0: "Intimidate", 1: "Moxie", H: "Sand Spit"},
+	},
+	krokorok: {
+		inherit: true,
+		abilities: {0: "Intimidate", 1: "Moxie", H: "Sand Spit"},
+	},
+	krookodile: {
+		inherit: true,
+		abilities: {0: "Intimidate", 1: "Moxie", H: "Sand Spit"},
+	},
+	sandygast: {
+		inherit: true,
+		abilities: {0: "Water Compaction", 1: "Sand Spit", H: "Sand Veil"},
+	},
+	palossand: {
+		inherit: true,
+		abilities: {0: "Water Compaction", 1: "Sand Spit", H: "Sand Veil"},
+	},
+	pyroar: {
+		inherit: true,
+		types: ["Fire", "Ground"],
+		abilities: {0: "Sand Rush", 1: "Unnerve", H: "Supreme Overlord"},
 	},
 };
