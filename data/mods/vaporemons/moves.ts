739d7621b0147e20362deee4e80de0e42596f2da
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-07-14 20:43:49 -0400

    Update moves.ts

--- data/mods/vaporemons/moves.ts
+++ data/mods/vaporemons/moves.ts
@@ -948,7 +948,15 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		},
 	},
 	psychicterrain: {
-		inherit: true,
+		num: 678,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Psychic Terrain",
+		pp: 10,
+		priority: 0,
+		flags: {nonsky: 1},
+		terrain: 'psychicterrain',
 		condition: {
 			duration: 5,
 			durationCallback(source, effect) {
@@ -962,9 +970,9 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				if (effect && (effect.priority <= 0.1 || effect.target === 'self')) {
 					return;
 				}
-				if (target.isSemiInvulnerable() || target.side === source.side) return;
+				if (target.isSemiInvulnerable() || target.isAlly(source)) return;
 				if (!target.isGrounded()) {
-					const baseMove = this.dex.getMove(effect.id);
+					const baseMove = this.dex.moves.get(effect.id);
 					if (baseMove.priority > 0) {
 						this.hint("Psychic Terrain doesn't affect Pokémon immune to Ground.");
 					}
@@ -989,22 +997,27 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 						}
 					}
 					this.debug('psychic terrain boost');
-					return this.chainModify([0x14CD, 0x1000]);
+					return this.chainModify([5325, 4096]);
 				}
 			},
-			onStart(battle, source, effect) {
+			onFieldStart(field, source, effect) {
 				if (effect?.effectType === 'Ability') {
-					this.add('-fieldstart', 'move: Psychic Terrain', '[from] ability: ' + effect, '[of] ' + source);
+					this.add('-fieldstart', 'move: Psychic Terrain', '[from] ability: ' + effect.name, '[of] ' + source);
 				} else {
 					this.add('-fieldstart', 'move: Psychic Terrain');
 				}
 			},
-			onResidualOrder: 21,
-			onResidualSubOrder: 2,
-			onEnd() {
+			onFieldResidualOrder: 27,
+			onFieldResidualSubOrder: 7,
+			onFieldEnd() {
 				this.add('-fieldend', 'move: Psychic Terrain');
 			},
 		},
+		secondary: null,
+		target: "all",
+		type: "Psychic",
+		zMove: {boost: {spa: 1}},
+		contestType: "Clever",
 	},
 	grassyterrain: {
 		inherit: true,
