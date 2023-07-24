04db821f3cf959deda9a003b805bc72dd781686c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-07-23 22:31:50 -0400

    Update formats-data.ts

--- data/mods/feou/formats-data.ts
+++ data/mods/feou/formats-data.ts
@@ -1,4 +1,4 @@
-export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	revarantis: {
 		tier: "FEOU",
 		doublesTier: "DOU",
