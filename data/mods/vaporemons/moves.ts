a00ed0ac30e8af9b857e963057a00ef66f440321
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-07-30 11:50:50 -0400

    Update moves.ts

--- data/mods/vaporemons/moves.ts
+++ data/mods/vaporemons/moves.ts
@@ -584,8 +584,9 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		flags: {protect: 1, mirror: 1, contact: 1},
 		multihit: 4,
 		onPrepareHit(target, source, move) {
-			target.addVolatile('substitute');
 			this.attrLastMove('[still]');
+			this.add('-anim', source, "Salt Cure", target);
+			target.addVolatile('substitute');
 			this.add('-anim', source, "Stone Axe", target);
 		},
 		secondary: null,
