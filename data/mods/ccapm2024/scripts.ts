2d274d9e1b8da5b2ba4ad3001ff9c674f8e8f670
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-03-30 23:04:06 -0400

    CCAPM Fix up scripts.ts Part 2

--- data/mods/ccapm2024/scripts.ts
+++ data/mods/ccapm2024/scripts.ts
@@ -1,22 +1,22 @@
 export const Scripts: ModdedBattleScriptsData = {
-	gen: 9,	
+	gen: 9,
 	actions: {
 		secondaries(targets: SpreadMoveTargets, source: Pokemon, move: ActiveMove, moveData: ActiveMove, isSelf?: boolean) {
 			if (!moveData.secondaries) return;
-			for (const target of targets) {
-				if (target === false) continue;
+			for (const foe of targets) {
+				if (foe === false) continue;
 				const secondaries: Dex.SecondaryEffect[] =
-					this.battle.runEvent('ModifySecondaries', target, source, moveData, moveData.secondaries.slice());
+					this.battle.runEvent('ModifySecondaries', foe, source, moveData, moveData.secondaries.slice());
 				for (const secondary of secondaries) {
 					const secondaryRoll = this.battle.random(100);
-					// User stat boosts or target stat drops can possibly overflow if it goes beyond 256 in Gen 8 or prior
+					// User stat boosts or foe stat drops can possibly overflow if it goes beyond 256 in Gen 8 or prior
 					const secondaryOverflow = (secondary.boosts || secondary.self) && this.battle.gen <= 8;
 					if (typeof secondary.chance === 'undefined' ||
 						secondaryRoll < (secondaryOverflow ? secondary.chance % 256 : secondary.chance)) {
 						let flag = true;
-						let canSetStatus = function (status, target, pokemon) {
+						const canSetStatus = function (status, target, pokemon) {
 							if (!target || target === false || target.status) return false;
-							let cantStatus = {
+							const cantStatus = {
 								brn: ['Fire', 'comatose', 'waterveil', 'waterbubble'],
 								frz: ['Ice', 'comatose', 'magmaarmor'],
 								par: ['Electric', 'comatose', 'limber'],
@@ -38,19 +38,19 @@ export const Scripts: ModdedBattleScriptsData = {
 							if (target.hasAbility(cantStatus[status])) return false;
 							return true;
 						};
-						if (moveData.secondary.status) flag = canSetStatus(moveData.secondary.status, target, source);
-						if (moveData.secondary.volatileStatus) flag = !(moveData.secondary.volatileStatus in target.volatiles);
-						if (moveData.secondary.volatileStatus === 'flinch') flag = flag && target.activeTurns && !target.moveThisTurn;
-						this.moveHit(target, source, move, secondary, true, isSelf);
-						if (moveData.secondary.self && moveData.secondary.self.boosts) {
+						if (moveData.secondary.status) flag = canSetStatus(moveData.secondary.status, foe, source);
+						if (moveData.secondary.volatileStatus) flag = !(moveData.secondary.volatileStatus in foe.volatiles);
+						if (moveData.secondary.volatileStatus === 'flinch') flag = flag && foe.activeTurns && !foe.moveThisTurn;
+						this.moveHit(foe, source, move, secondary, true, isSelf);
+						if (moveData.secondary.self.boosts) {
 							Object.keys(moveData.secondary.self.boosts).forEach(boost => {
 								if (source.boosts[boost] === 6) flag = false;
 							});
 						} else {
-							flag = flag && !(target.hp === undefined || target.hp <= 0);
+							flag = flag && !(foe.hp === undefined || foe.hp <= 0);
 						}
 						if (moveData.target !== 'self' && moveData.secondary.boosts) {
-							let cantLower = {
+							const cantLower = {
 								'atk': ['clearbody', 'fullmetalbody', 'hypercutter', 'whitesmoke'],
 								'def': ['bigpecks', 'clearbody', 'fullmetalbody', 'whitesmoke'],
 								'spa': ['clearbody', 'fullmetalbody', 'whitesmoke'],
@@ -58,45 +58,45 @@ export const Scripts: ModdedBattleScriptsData = {
 								'spe': ['clearbody', 'fullmetalbody', 'whitesmoke'],
 								'accuracy': ['clearbody', 'fullmetalbody', 'keeneye', 'whitesmoke'],
 							};
-							for (let k in moveData.secondary.boosts) {
-								if (target.boosts[k] === -6) {
+							for (const k in moveData.secondary.boosts) {
+								if (foe.boosts[k] === -6) {
 									flag = false;
 									continue;
 								}
-								if (moveData.secondary.boosts[k] < 0 && target.hasAbility(cantLower[k]) && !move.ignoreAbility) {
+								if (moveData.secondary.boosts[k] < 0 && foe.hasAbility(cantLower[k]) && !move.ignoreAbility) {
 									flag = false;
 									break;
 								}
 							}
 						}
 						if (source.hasAbility('sheerforce')) flag = false;
-						if (target && target.hasAbility('shielddust') && !move.ignoreAbility && !move.secondary.self.boosts) {
+						if (foe && foe.hasAbility('shielddust') && !move.ignoreAbility && !move.secondary.self.boosts) {
 							flag = false;
 						}
-						if (flag && target.hasAbility('countermeasures')) {
-							this.battle.add('-activate', target, 'ability: Countermeasures');
-							this.battle.damage(source.baseMaxhp * (100 - secondary.chance) / 100, source, target);
+						if (flag && foe.hasAbility('countermeasures')) {
+							this.battle.add('-activate', foe, 'ability: Countermeasures');
+							this.battle.damage(source.baseMaxhp * (100 - secondary.chance) / 100, source, foe);
 						}
 					}
 				}
 			}
-		}
+		},
 	},
 	side: {
 		addFishingTokens(amount: number) {
-			if(amount === 0) return;
-			if(this.fishingTokens === undefined) this.fishingTokens = 0;
-			if(this.battle.field.isTerrain('fishingterrain')) amount *= 2;
+			if (amount === 0) return;
+			if (this.fishingTokens === undefined) this.fishingTokens = 0;
+			if (this.battle.field.isTerrain('fishingterrain')) amount *= 2;
 			this.fishingTokens += amount;
 			const word = (amount === 1) ? 'token was' : 'tokens were';
 			this.battle.add('-message', `${amount} fishing ${word} added to ${this.name}'s side!`);
 			this.battle.hint(`They now have ${this.fishingTokens} tokens.`);
 		},
 		removeFishingTokens(amount: number) {
-			if(amount === 0) return;
-			if(this.fishingTokens === undefined) this.fishingTokens = 0;
+			if (amount === 0) return;
+			if (this.fishingTokens === undefined) this.fishingTokens = 0;
 			if (amount > this.fishingTokens) {
-				//this.add('-message', `There weren't enough fishing tokens on the field!`);
+				// this.add('-message', `There weren't enough fishing tokens on the field!`); (remove later)
 				return false;
 			}
 			this.fishingTokens -= amount;
