7304ed50ac0ab95852f7ab0740311fa4e5222b9e
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-04-22 21:24:11 -0400

    Final Clean Up Pt. 4

--- data/mods/ccapm2024/moves.ts
+++ data/mods/ccapm2024/moves.ts
@@ -11,33 +11,6 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 	},
 
 	// fake moves
-	/* medic: {
-		accuracy: true,
-		basePower: 0,
-		category: "Status",
-		name: "medic",
-		pp: 20,
-		priority: 0,
-		flags: { reflectable: 1, nonsky: 1, mustpressure: 1, nosketch: 1 },
-		sideCondition: 'medic',
-		condition: {
-			// this is a side condition
-			onSideStart(side) {
-				this.add('-sidestart', side, 'medic', '[silent]');
-			},
-			onEntryHazard(pokemon) {
-				this.heal(pokemon.maxhp / 6);
-				if (pokemon.status) pokemon.cureStatus();
-				pokemon.side.removeSideCondition('medic');
-				this.add('-sideend', pokemon.side, 'move: Medic', '[silent]');
-			},
-		},
-		secondary: null,
-		target: "allySide",
-		type: "Normal",
-		zMove: { boost: { def: 1 } },
-		contestType: "Clever",
-	}, */
 	fishingtokens: {
 		accuracy: true,
 		basePower: 0,
