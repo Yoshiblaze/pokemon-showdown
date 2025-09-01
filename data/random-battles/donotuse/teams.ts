d43c99435e38d58116026a84a5b336e8cfbd997b
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-09-01 15:33:07 -0400

    Fix Hidden Power IVs Pt. 2

--- data/random-battles/donotuse/teams.ts
+++ data/random-battles/donotuse/teams.ts
@@ -227,6 +227,8 @@ export class RandomDNUTeams extends RandomTeams {
 			['dazzlinggleam', 'thunderwave'],
 			// Combee
 			['lunge', 'bugbuzz'],
+			// Nidoran-F
+			['sludgebomb', 'poisonjab'],
 		];
 
 		for (const pair of incompatiblePairs) this.incompatibleMoves(moves, movePool, pair[0], pair[1]);
@@ -783,20 +785,24 @@ export class RandomDNUTeams extends RandomTeams {
 			ivs.atk = 0;
 		}
 
+		if (moves.has('gyroball') || moves.has('trickroom') || moves.has('archaicglare')) {
+			evs.spe = 0;
+			ivs.spe = 0;
+
 		// Hidden Power Grass IVs
-		if (species.id === 'luvdisc') {
+		if (species.id === 'luvdisc' && moves.has('hiddenpower')) {
 			ivs.atk = 0;
 			ivs.spa = 30;
 		}
 
 		// Hidden Power Psychic IVs
-		if (species.id === 'unown') {
+		if (species.id === 'unown' && moves.has('hiddenpower')) {
 			ivs.atk = 0;
 			ivs.spe = 30;
 		}
 
 		// Hidden Power Fire IVs
-		if (['fomantis', 'nincada', 'petilil'].includes(species.id)) {
+		if (['fomantis', 'nincada', 'petilil'].includes(species.id) && moves.has('hiddenpower')) {
 			ivs.atk = 0;
 			ivs.spa = 30;
 			ivs.spe = 30;
