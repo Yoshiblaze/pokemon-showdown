071d57e1cd6fb50c035664beee4e7a2e51cd82d7
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-01-05 21:28:27 -0500

    Update rulesets.ts

--- data/mods/ccapm2025/rulesets.ts
+++ data/mods/ccapm2025/rulesets.ts
@@ -1,3 +1,15 @@
 export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable = {
-
+	terastalclause: {
+		effectType: 'Rule',
+		name: 'Terastal Clause',
+		desc: "Prevents Pok&eacute;mon from Terastallizing",
+		onBegin() {
+			for (const pokemon of this.getAllPokemon()) {
+				if (pokemon.species.baseSpecies !== 'Ogerpon') {
+					pokemon.canTerastallize = null;
+				}
+			}
+			this.add('rule', 'Terastal Clause: You cannot Terastallize');
+		},
+	},
 };
