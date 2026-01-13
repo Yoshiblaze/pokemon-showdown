676275fbb75957f2193a3fe6f511c237f3fd1171
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-01-13 01:14:31 -0500

    Update formats-data.ts

--- data/mods/ccapm2025/formats-data.ts
+++ data/mods/ccapm2025/formats-data.ts
@@ -1,6 +1,331 @@
 export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
-  // mostly for advent mons
+	syclar: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	syclant: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	revenankh: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	embirch: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	flarelm: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	pyroak: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	breezi: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	fidgit: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	rebble: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	tactite: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	stratagem: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	privatyke: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	arghonaut: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	nohface: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	kitsunoh: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	monohm: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	duohm: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	cyclohm: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	dorsoil: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	colossoil: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	protowatt: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	krilowatt: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	voodoll: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	voodoom: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	scratchet: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	tomohawk: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	necturine: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	necturna: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	mollux: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	cupra: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	argalis: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	aurumoth: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	brattler: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	malaconda: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	cawdet: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	cawmodore: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	volkritter: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	volkraken: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	snugglow: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	plasmanta: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	floatoy: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	caimanoe: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	naviathan: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	crucibelle: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	crucibellemega: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	pluffle: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	kerfluffle: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	pajantom: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	mumbao: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	jumbao: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	fawnifer: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	electrelk: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	caribolt: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	smogecko: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	smoguana: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	smokomodo: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	swirlpool: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	coribalis: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	snaelstrom: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	justyke: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	equilibra: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	solotl: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	astrolotl: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	miasmite: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	miasmaw: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	chromera: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	venomicon: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	venomiconepilogue: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	saharascal: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	saharaja: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	ababo: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	scattervein: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	hemogoblin: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	cresceidon: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	chuggon: {
+		isNonstandard: "Custom",
+		tier: "CAP LC",
+	},
+	draggalong: {
+		isNonstandard: "Custom",
+		tier: "CAP NFE",
+	},
+	chuggalong: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	shox: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	ramnarok: {
+		isNonstandard: "Custom",
+		tier: "CAP",
+	},
+	ramnarokradiant: {
+		isNonstandard: "Custom",
+		tier: "Illegal",
+	},
+
+	// mostly for advent mons
 	ogerponpixiedust: {
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	shakite: {
 		tier: "OU",
 		doublesTier: "DOU",
 	},
