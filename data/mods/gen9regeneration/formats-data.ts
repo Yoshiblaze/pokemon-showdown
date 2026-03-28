f8af5559e02b8f1fb1e3c6819f52d4dadd7b57b0
Yoshiblaze <rosefias@gmail.com>
2026-03-27 22:02:53 -0400

    ReGeneration: Initial Commit

---
+++ data/mods/gen9regeneration/formats-data.ts
@@ -0,0 +1,674 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	bulbasaur: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	ivysaur: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	venusaur: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	venusaurmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	charmander: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	charmeleon: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	charizard: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	charizardmegax: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	charizardmegay: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	squirtle: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	wartortle: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	blastoise: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	blastoisemega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	caterpie: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	metapod: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	butterfree: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	weedle: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	kakuna: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	beedrill: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	beedrillmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	pidgey: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	pidgeotto: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	pidgeot: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	pidgeotmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	rattata: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	raticate: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	spearow: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	fearow: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	ekans: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	arbok: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	pikachu: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	raichu: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	sandshrew: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	sandslash: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	nidoranf: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	nidorina: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	nidoqueen: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	nidoranm: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	nidorino: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	nidoking: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	cleffa: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	clefairy: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	clefable: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	vulpix: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	ninetales: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	igglybuff: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	jigglypuff: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	wigglytuff: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	zubat: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	golbat: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	oddish: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	gloom: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	vileplume: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	paras: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	parasect: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	venonat: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	venomoth: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	diglett: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	dugtrio: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	meowth: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	persian: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	psyduck: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	golduck: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	mankey: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	primeape: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	growlithe: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	arcanine: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	poliwag: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	poliwhirl: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	poliwrath: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	abra: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	kadabra: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	alakazam: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	alakazammega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	machop: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	machoke: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	machamp: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	bellsprout: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	weepinbell: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	victreebel: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	tentacool: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	tentacruel: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	geodude: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	graveler: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	golem: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	ponyta: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	rapidash: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	slowpoke: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	slowbro: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	slowbromega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	magnemite: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	magneton: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	farfetchd: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	doduo: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	dodrio: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	seel: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	dewgong: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	grimer: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	muk: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	shellder: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	cloyster: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	gastly: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	haunter: {
+	  tier: "NFE",
+		doublesTier: "NFE",
+	},
+	gengar: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	gengarmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	onix: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	drowzee: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	hypno: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	krabby: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	kingler: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	voltorb: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	electrode: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	exeggcute: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	exeggutor: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	cubone: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	marowak: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	hitmonlee: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	hitmonchan: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	lickitung: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	koffing: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	weezing: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	rhyhorn: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	rhydon: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	chansey: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	tangela: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	kangaskhan: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	kangaskhanmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	horsea: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	seadra: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	goldeen: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	seaking: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	staryu: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	starmie: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	mrmime: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	scyther: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	jynx: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	electabuzz: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	magmar: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	pinsir: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	pinsirmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	tauros: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	magikarp: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	gyarados: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	gyaradosmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	lapras: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	ditto: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	eevee: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	vaporeon: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	jolteon: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	flareon: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	porygon: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	omanyte: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	omastar: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	kabuto: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	kabutops: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	aerodactyl: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	aerodactylmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	snorlax: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	articuno: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	zapdos: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	moltres: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	dratini: {
+		tier: "LC",
+		doublesTier: "LC",
+	},
+	dragonair: {
+		tier: "NFE",
+		doublesTier: "NFE",
+	},
+	dragonite: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+	mewtwo: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	mewtwomegax: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	mewtwomegay: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+	},
+	mew: {
+		tier: "OU",
+		doublesTier: "OU",
+	},
+};
---
+++ data/mods/gen9regeneration/moves.ts
@@ -0,0 +1,377 @@
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	// new
+	vitalenergy: {
+		accuracy: 100,
+		basePower: 150,
+		basePowerCallback(pokemon, target, move) {
+			return move.basePower * pokemon.hp / pokemon.maxhp;
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Seed Flare", target);
+		},
+		category: "Special",
+		name: "Vital Energy",
+		shortDesc: "Less power as user's HP decreases. Hits foe(s).",
+		pp: 5,
+		priority: 0,
+		flags: { protect: 1, mirror: 1 },
+		secondary: null,
+		target: "allAdjacentFoes",
+		type: "Grass",
+		contestType: "Beautiful",
+	},
+	smokytorment: {
+		accuracy: true,
+		basePower: 75,
+		category: "Physical",
+		name: "Smoky Torment",
+		shortDesc: "Applies the Torment effect on opponent.",
+		pp: 10,
+		priority: 0,
+		flags: {},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Outrage", target);
+		},
+		secondary: {
+			chance: 100,
+			volatileStatus: 'torment',
+		},
+		target: "normal",
+		type: "Dark",
+		contestType: "Cool",
+	},
+	powerwash: {
+		accuracy: 100,
+		basePower: 50,
+		category: "Special",
+		name: "Power Wash",
+		shortDesc: "Removes all hazards in the field. If any are cleared, the user heals for 50% of its maximum HP.",
+		pp: 40,
+		priority: 0,
+		flags: { contact: 1, protect: 1, mirror: 1 },
+		onHit(target, source, move) {
+			let success = false;
+			const removeAll = [
+				'spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge',
+			];
+			for (const sideCondition of removeAll) {
+				if (source.side.removeSideCondition(sideCondition)) {
+					this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name, '[from] move: Power Wash', `[of] ${source}`);
+					success = true;
+				}
+			}
+			this.heal(Math.ceil(source.maxhp * 0.5), source);
+			return success;
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Volt Switch", target);
+		},
+		target: "normal",
+		type: "Water",
+		contestType: "Cool",
+	},
+	brainwave: {
+		accuracy: 100,
+		basePower: 70,
+		category: "Special",
+		name: "Brainwave",
+		shortDesc: "Uses user's Special Defense stat as Special Attack in damage calculation.",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1 },
+		useSourceDefensiveAsOffensive: true,
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Expanding Force", target);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Psychic",
+	},
+	illwind: {
+		accuracy: 100,
+		basePower: 100,
+		category: "Special",
+		name: "Ill Wind",
+		shortDesc: "Lowers the user's Sp. Atk by 1, drains 25% damage, heals another 25% for each neg SpA boost.",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, heal: 1 },
+		self: {
+			boosts: {
+				spa: -1,
+			},
+		},
+		drain: [1, 4],
+		onModifyMove(move, pokemon) {
+			move.drain = [pokemon.negativeBoosts.spa(), 4];
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Icy Wind", target);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Ice",
+		contestType: "Clever",
+	},
+	guardiandive: {
+		accuracy: 100,
+		basePower: 75,
+		category: "Physical",
+		name: "Guardian Dive",
+		shortDesc: "Uses user's Defense stat as Attack in damage calculation.",
+		pp: 10,
+		priority: 0,
+		flags: { contact: 1, protect: 1, mirror: 1 },
+		useSourceDefensiveAsOffensive: true,
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Acrobatics", target);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Flying",
+	},
+	coralcrash: {
+		accuracy: 100,
+		basePower: 120,
+		category: "Physical",
+		name: "Coral Crash",
+		shortDesc: "33% recoil. 10% chance to poison.",
+		pp: 15,
+		priority: 0,
+		flags: { protect: 1, mirror: 1 },
+		secondary: {
+			chance: 10,
+			status: 'psn',
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Aqua Tail", target);
+		},
+		recoil: [33, 100],
+		target: "normal",
+		type: "Poison",
+		contestType: "Tough",
+	},
+	slipaway: {
+		accuracy: 100,
+		basePower: 50,
+		category: "Physical",
+		name: "Slip Away",
+		shortDesc: "100% chance to lower the target's Attack by 1, switches the user out.",
+		pp: 20,
+		priority: 0,
+		flags: { contact: 1, protect: 1, mirror: 1, authentic: 1 },
+		secondary: {
+			chance: 100,
+			boosts: {
+				atk: -1,
+			},
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Aqua Tail", target);
+		},
+		selfSwitch: true,
+		target: "normal",
+		type: "Water",
+		zMove: {effect: 'healreplacement'},
+		contestType: "Cool",
+	},
+	flareout: {
+		accuracy: 100,
+		basePower: 70,
+		category: "Physical",
+		name: "Flare Out",
+		shortDesc: "100% chance to lower the target's Defense by 1, switches the user out.",
+		pp: 20,
+		priority: 0,
+		flags: { contact: 1, protect: 1, mirror: 1, authentic: 1 },
+		secondary: {
+			chance: 100,
+			boosts: {
+				def: -1,
+			},
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Flame Charge", target);
+		},
+		selfSwitch: true,
+		target: "normal",
+		type: "Fire",
+		zMove: {effect: 'healreplacement'},
+		contestType: "Cool",
+	},
+	buzzoff: {
+		accuracy: 100,
+		basePower: 60,
+		category: "Special",
+		name: "Buzz Off",
+		shortDesc: "100% chance to lower the target's Speed by 1, switches the user out.",
+		pp: 20,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, authentic: 1 },
+		secondary: {
+			chance: 100,
+			boosts: {
+				spe: -1,
+			},
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Volt Switch", target);
+		},
+		selfSwitch: true,
+		target: "normal",
+		type: "Electric",
+		zMove: {effect: 'healreplacement'},
+		contestType: "Cool",
+	},
+	powdergale: {
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Powder Gale",
+		shortDesc: "100% chance to poison. Harshly lowers a random one of the target's stat.",
+		pp: 30,
+		priority: 0,
+		flags: {},
+		onHit(target) {
+			const stats: BoostID[] = [];
+			let stat: BoostID;
+			for (stat in target.boosts) {
+				if (stat === 'accuracy' || stat === 'evasion') continue;
+				if (target.boosts[stat] < 6) {
+					stats.push(stat);
+				}
+			}
+			if (stats.length) {
+				const randomStat = this.sample(stats);
+				const boost: SparseBoostsTable = {};
+				boost[randomStat] = -2;
+				this.boost(boost);
+			} else {
+				return;
+			}
+		},
+		secondary: {
+			chance: 100,
+			status: 'psn',
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Hurricane", target);
+		},
+		target: "normal",
+		type: "Bug",
+		zMove: {effect: 'crit2'},
+		contestType: "Tough",
+	},
+	splashback: {
+		accuracy: 100,
+		basePower: 50,
+		basePowerCallback(pokemon) {
+			return Math.min(300, 50 + 50 * pokemon.timesAttacked);
+		},
+		onEnd(pokemon) {
+			this.add('-end', pokemon, 'Splashback', '[silent]');
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Flip Turn", target);
+		},
+		category: "Physical",
+		name: "Splashback",
+		shortDesc: "Base power increases by 50 every time this Pokemon is hit. Max 300 BP. Reset on switch out.",
+		pp: 10,
+		priority: 0,
+		flags: { contact: 1, protect: 1, mirror: 1, punch: 1 },
+		secondary: null,
+		target: "normal",
+		type: "Water",
+	},
+	ragingtorrent: {
+		accuracy: 100,
+		basePower: 90,
+		category: "Physical",
+		name: "Raging Torrent",
+		shortDesc: "Lowers the target's Atk by 1. Inflicts Encore on the user.",
+		pp: 20,
+		priority: 0,
+		flags: { contact: 1, protect: 1, mirror: 1 },
+		self: {
+		   volatileStatus: 'encore',
+		},
+		secondary: {
+			chance: 100,
+			boosts: {
+				atk: -1,
+			},
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Liquidation", target);
+		},
+		target: "allAdjacentFoes",
+		type: "Water",
+		contestType: "Tough",
+	},
+	familyonslaught: {
+		accuracy: 100,
+		basePower: 40,
+		category: "Physical",
+		shortDesc: "Hits 2-4 times.",
+		name: "Family Onslaught",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1 },
+		multihit: [2, 4],
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Population Bomb", target);
+		},
+		target: "normal",
+		type: "Normal",
+		contestType: "Cute",
+	},
+	pestspread: {
+		accuracy: 100,
+		basePower: 100,
+		category: "Physical",
+		shortDesc: "Changes the target's type to the user's type.",
+		name: "Pest Spread",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1 },
+		onAfterHit(target, source) {
+			if (target.species && (target.species.num === 493 || target.species.num === 773)) return false;
+			if (target.terastallized) return false;
+			const oldApparentType = target.apparentType;
+			let newBaseTypes = source.getTypes(true).filter(type => type !== '???');
+			if (!newBaseTypes.length) {
+				if (source.addedType) {
+					newBaseTypes = ['Normal'];
+				} else {
+					return false;
+				}
+			}
+			this.add('-start', target, 'typechange', '[from] move: Pest Spread', `[of] ${target}`);
+			target.setType(newBaseTypes);
+			target.addedType = source.addedType;
+			target.knownType = source.isAlly(source) && source.knownType;
+			if (!target.knownType) target.apparentType = oldApparentType;
+		},
+		onPrepareHit(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Sludge Bomb", target);
+		},
+		target: "normal",
+		type: "Poison",
+	},
+};
