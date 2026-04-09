57831cc7d8854b6a3cadf51ce9a4a24c680309e9
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-04-08 22:29:22 -0400

    merge stak's abilities 2

--- data/mods/ccapm2025/scripts.ts
+++ data/mods/ccapm2025/scripts.ts
@@ -357,7 +357,8 @@ export const Scripts: ModdedBattleScriptsData = {
 			if (item === 'ironball') return true;
 			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
 			if (!negateImmunity && this.hasType('Flying') && !(this.hasType('???') && 'roost' in this.volatiles)) return false;
-			if ((this.hasAbility('levitate') || this.hasAbility('interdimensionalmissile')) &&
+			if ((this.hasAbility('levitate') || this.hasAbility('interdimensionalmissile') ||
+				this.hasAbility('stackchange')) &&
 				!this.battle.suppressingAbility(this)) {
 				return null;
 			}
